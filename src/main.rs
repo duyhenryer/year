cb63404f70f07317c5909e596620f24b4bac7f21
duyhenryer <duyhenry250897@gmail.com>
2024-06-26 16:49:09 +0700

    Update code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use chrono::{Datelike, TimeZone, Utc};
+use std::fs::OpenOptions;
+use std::io::{self, Write};
 use std::time::{SystemTime, UNIX_EPOCH};
 
 mod progress_bar;
 use progress_bar::generate_progress_bar;
 
-fn main() {
+fn main() -> io::Result<()> {
     let this_year = Utc::now().year();
     let start_time_of_this_year = Utc.with_ymd_and_hms(this_year, 1, 1, 0, 0, 0).unwrap().timestamp_millis();
     let end_time_of_this_year = Utc.with_ymd_and_hms(this_year, 12, 31, 23, 59, 59).unwrap().timestamp_millis();
@@ -14,14 +16,28 @@ fn main() {
     let progress_bar_of_this_year = generate_progress_bar(progress_of_this_year);
 
     let readme = format!(
-        "### Hi there 👋\n\n\
-        ⏳ Year Progress {} {:.2} %\n\n\
-        ---\n\n\
-        ⏰ Updated on {}\n\n\
+        "\n\
+        ⏳ Year Progress {} {:.2} %\n\
+        ⏰ {}\n\n\
         ",
         progress_bar_of_this_year,
         progress_of_this_year * 100.0,
         Utc::now().to_rfc2822()
     );
+
+    let readme_path = "README.md";
+
+    // Open README.md in write mode, create it if it doesn't exist
+    let mut file = OpenOptions::new()
+        .create(true)
+        .truncate(true) // Truncate to clear old content
+        .write(true)
+        .open(readme_path)?;
+
+    // Write the new content to README.md
+    file.write_all(readme.as_bytes())?;
+
     println!("{}", readme);
+
+    Ok(())
 }
