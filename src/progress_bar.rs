ef3bd94a62ee39bbe454a7fe2e3fe2a126ce197f
Duy Henry <duyhenry250897@gmail.com>
2024-06-28 07:41:36 +0000

    update

--- src/progress_bar.rs
+++ src/progress_bar.rs
@@ -1,6 +1,6 @@
 pub fn generate_progress_bar(progress: f64) -> String {
     let progress_bar_capacity = 30;
     let passed_progress_bar_index = (progress * progress_bar_capacity as f64) as usize;
-    let progress_bar = "█".repeat(passed_progress_bar_index) + &"▁".repeat(progress_bar_capacity - passed_progress_bar_index);
+    let progress_bar = "█".repeat(passed_progress_bar_index) + &"░".repeat(progress_bar_capacity - passed_progress_bar_index);
     format!("{{ {} }}", progress_bar)
 }
